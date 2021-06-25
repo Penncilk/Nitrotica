ac50ee6f330658c7090891dd00211ea3e5c64e61
Alekzander Gottselig <alekz.gottselig@gmail.com>
2021-06-24 18:50:51 -0600

    Update Player.hx

--- source/Player.hx
+++ source/Player.hx
@@ -1,29 +1,33 @@
 package;
 
+import flixel.FlxG;
+import flixel.FlxObject;
 import flixel.FlxSprite;
 import flixel.FlxState;
-import flixel.FlxObject;
-import flixel.FlxG;
 import flixel.util.FlxColor;
 import flixel.util.FlxSpriteUtil;
 
+class Player extends FlxSprite
+{
+	public var player = new FlxSprite();
 
-class Player extends FlxSprite {
-    public var player = new FlxSprite();
-function movehorizontal()
-    {
-        if (FlxG.keys.pressed.RIGHT && !FlxG.keys.pressed.LEFT)
-            {
-                    player.acceleration.x = 500;
-            }
-        
-            if (FlxG.keys.pressed.LEFT && !FlxG.keys.pressed.RIGHT)
-            {
-                    player.acceleration.x = -500;
-            }
-            if (!FlxG.keys.pressed.LEFT && !FlxG.keys.pressed.RIGHT || FlxG.keys.pressed.LEFT && FlxG.keys.pressed.RIGHT) {
-            player.velocity.x = 0;
-            player.acceleration.x = 0;
-            }
-    }
+	function movehorizontal()
+	{
+		if (FlxG.keys.pressed.RIGHT && !FlxG.keys.pressed.LEFT)		
+		{
+			player.acceleration.x = 500;
+		}
+
+		if (FlxG.keys.pressed.LEFT && !FlxG.keys.pressed.RIGHT)
+		{
+			player.acceleration.x = -500;
+		}
+			
+        if (!FlxG.keys.pressed.LEFT && !FlxG.keys.pressed.RIGHT || FlxG.keys.pressed.LEFT && FlxG.keys.pressed.RIGHT)
+		{
+			player.velocity.x = 0;
+			player.acceleration.x = 0;
+		}
+		
+	}
 }
